ce834517c82d2c4ef456be831dfe70cc474a019e
Marianne Webb <mvwebb88@hotmail.com>
2026-02-11 14:10:16 -0500

    Fix HootList rendering and connect hoots GET to UI

--- src/components/HootList/HootList.jsx
+++ src/components/HootList/HootList.jsx
@@ -1,24 +1,30 @@
-import { Link } from 'react-router'
+import { Link } from "react-router-dom"
 
-const HootList = ({ hoots }) => {
-    return (
-        <main>
-            {hoots.map((hoot) => (
-                <Link key={hoot.id} to={`/hoots/${hoot.id}`}>
-                    <article>
-                        <header>
-                            <h2>{hoot.title}</h2>
-                            <p>
-                                {`${hoot.author_username} posted on
-                ${new Date(hoot.created_at).toLocaleDateString()}`}
-                            </p>
-                        </header>
-                        <p>{hoot.text}</p>
-                    </article>
-                </Link>
-            ))}
-        </main>
-    )
+const HootList = ({ hoots = [] }) => {
+  if (!hoots.length) {
+    return <p>No hoots yet.</p>
+  }
+
+  return (
+    <main>
+      {hoots.map((hoot) => (
+        <article key={hoot.id}>
+          <Link to={`/hoots/${hoot.id}`}>
+            <h2>{hoot.title}</h2>
+          </Link>
+
+          <p>
+            Posted by {hoot.author_username} on{" "}
+            {new Date(hoot.created_at).toLocaleDateString()}
+          </p>
+
+          <p>{hoot.content}</p>
+        </article>
+      ))}
+    </main>
+  )
 }
 
 export default HootList
+
+
